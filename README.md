725a2e52b10f378068fb9208f85d2b821691c1c4
Jeppe Knockaert <jeppe.knockaert@gmail.com>
2013-10-16 22:41:00 +0200

    Basic server functionality added, updated readme with usage info

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ $ npm install
 ```
 
 Node will automatically fetch all dependencies for the project.
+
+##Usage
+
+In order to start the server application, use the following command in your main folder
+
+``` bash
+$ node server/server.js
+```
+
+This will start the server on port 4000.
--- server/server.js
+++ server/server.js
@@ -4,3 +4,19 @@
 
 // Load "express" (web application framework for node)
 var express = require('express');
+
+// Create a new application
+var app = express();
+
+// If a user surfs to the root folder, send him to index file in the client folder
+app.get('/', function(req, res){
+  res.sendfile('client/index.html');
+});
+
+// If a JavaScript or CSS file is requested, send the request to the client folder
+app.get(/^(\/(js|css)\/.+)$/, function(req, res) {
+  res.sendfile('client/' + req.params[0]);
+});
+
+// Let the server listen for incoming requests on port 4000
+app.listen(4000);
